f00c825d5ab43a82ec796995c3555f93b4381a43
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-06-09 17:26:41 +0200

    feat: add yaib concepts

--- scripts/import.R
+++ scripts/import.R
@@ -3,7 +3,16 @@ install.packages("units")
 
 Sys.setenv(RICU_DATA_PATH = "/var/data")
 
-ricu::import_src("eicu")
+library(ricu)
+
+source("ricu/callbacks/callback-icu-mortality.R")
+source("ricu/callbacks/callback-kdigo.R")
+source("ricu/callbacks/callback-sepsis.R")
+
+concept_path <- file.path("ricu", "configs", c("chemistry", "circulatory", "demographics", "hematology", "medications", "misc", "outcomes", "output", "vitals"))
+dict <- load_dictionary(cfg_dirs = concept_path)
+
+import_src("eicu")
 # ricu::import_src("miiv")
 # ricu::import_src("hirid")
 # ricu::import_src("aumc")
